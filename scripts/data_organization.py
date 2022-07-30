6da6b5f01fcaef118e3e48d57b8645bcfa5d6a30
adam pere <47603073+adamp-lvthn@users.noreply.github.com>
2022-07-29 23:07:36 -0500

    added functons to split the large csv; updated list_to_json to properly format scrambles to words; some proper clean up

--- scripts/data_organization.py
+++ scripts/data_organization.py
@@ -6,14 +6,14 @@ from typing import List
 # FUNCTIONS
 # ===============
 
-#converts a file with a word on every line to a json object
-def list_to_json(file_path):
+#converts a file with a word on every line into a json object
+def file_to_json(file_path):
     words = open(file_path)
     word_list = words.readlines()
     scrambles = {}
 
-    for count in range(len(word_list)):
-        word = word_list[count].rstrip()
+    for i in range(len(word_list)):
+        word = word_list[i].rstrip()
 
         start_index = 0
         end_index = len(word)
@@ -51,6 +51,7 @@ def generate_permutations(word, start_i, end_i) -> List:
 
     return permutations
 
+
 def list_to_string(list):
     return ''.join(list)
 
@@ -58,20 +59,83 @@ def list_to_string(list):
 def write_to_file(file_path, text):
     file = open(file_path, 'w')
     file.write(text)
+    file.close()
+
+def write__lines_to_file(file_path, text):
+    file = open(file_path, 'w')
+    file.writelines(text)
+    file.close()
+
+# Takes a
+def split_file(file_name:str, source_path:str, target_path:str, file_extension:str, split_max:int ):
+    count = 0
+    source = '{0}/{1}.{2}'.format(source_path, file_name,  file_extension, 'r')
+    source_file = open(source, 'r')
+    target = '{0}/{1}_{2}.{3}'.format(target_path, file_name, count, file_extension)
+    target_file = open(target, 'w')
+    temp_data = []
+
+
+    source_text = source_file.readlines()
+    source_file.close()
+    leftover = False
+
+    for i in range(1, len(source_text)):
+        if not i == 1 and i%split_max == 1 :
+
+            # File done
+            # write and close current file
+            target_file.writelines(temp_data)
+            target_file.close()
+            leftover = False
+
+            if not i == len(source_text) -1:
+                # refresh for new file
+                count = count + 1
+                temp_data = []
+                target ='{0}/{1}_{2}.{3}'.format(target_path, file_name, count, file_extension)
+                target_file = open(target, 'w')
+        else:
+            # collect data
+            temp_data.append(source_text[i])
+            leftover = True
+    if leftover:
+        target_file.writelines(temp_data)
+        target_file.close()
+
+
+
+
+
 
 # ===============
 # Environment Variables
 # ===============
 TEST = 0
 PRODUCTION = 1
-FILES = ['words_short.csv', 'words.csv']
+FILES = ['words_short', 'words']
 
 # ===============
 # Commands
 # ===============
-file = FILES[PRODUCTION]
-words = list_to_json(sys.path[0]+'/../data/' + file)
+
+
+# split files
+file_name = FILES[TEST]
+src_file_path = '{0}/../data/'.format(sys.path[0])
+target_file_path = '{0}/../data/{1}'.format(sys.path[0], file_name)
+file_extension = 'csv'
+split_max = 3
+
+split_file(file_name, src_file_path, target_file_path, file_extension, split_max)
 
 print('========================')
-write_to_file(sys.path[0]+'/../data/' + file, json.dumps(words))
-print(json.dumps(words))
+
+#words = file_to_json(sys.path[0]+'/../data/' + file)
+#write_to_file(sys.path[0]+'/../data/' + file, json.dumps(words))
+#print(json.dumps(words))
+
+
+# create directiory of file_name_json
+# count files in directory
+# loop through files and convert to json, wite to file
