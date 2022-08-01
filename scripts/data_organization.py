651f1b940fdf601c7ea8825f7bf50628c22fb327
adam pere <47603073+adamp-lvthn@users.noreply.github.com>
2022-07-31 19:23:10 -0500

    updated to use mongodb instead of stupid files

--- scripts/data_organization.py
+++ scripts/data_organization.py
@@ -4,12 +4,12 @@ import os
 import time
 from itertools import permutations
 from typing import List
+from pymongo import MongoClient
+import pymongo
 
 # ===============
 # FUNCTIONS
 # ===============
-
-
 
 def list_to_string(list)-> str:
     """
@@ -90,13 +90,13 @@ def split_file(file_name:str, source_path:str, target_path:str, file_extension:s
                 force_new_file = True
     if leftover:
         file_contents.append(temp_data)
-    
+
     target_file.writelines(json.dumps(file_contents))
     target_file.close()
 
     if i%100 == 0 or i == len(source_text) - 1:
         print('---------------- {0} Files Created'.format(count))
-        
+
     print('---- Done: Splitting Files ----')
 
 
@@ -105,7 +105,7 @@ def files_to_json(directory_name:str, src_data, target_directory_path:str, proce
 
     count = 0
     print('got files')
-    
+
     total_files = len(src_data)
     chunk_size = total_files / process_count
 
@@ -119,39 +119,47 @@ def files_to_json(directory_name:str, src_data, target_directory_path:str, proce
         is_file_good_a = count >= min_idx and count < max_idx
         is_file_good_b = process_id == process_count - 1 and count > max_idx
         is_file_good = is_file_good_a or is_file_good_b
-        
+
         if is_file_good:
-            #src_file = os.path.join(src_directory_path, file_name)
+            src_file = os.path.join(src_directory_path, file_name)
             target_file = os.path.join(target_directory_path, directory_name + '_'+ str(count) + '.json')
 
             print('{0} | Reading from file: {1}'.format(count, cur_data))
             words_json = file_to_json(cur_data)
 
             print('{0} | Writing to file: {1}'.format(count, target_file))
-            write_to_file(target_file, json.dumps(words_json))
+            #write_to_file(target_file, json.dumps(words_json))
         count = count + 1
     print('---- DONE: Creating JSON Files ----')
 
 #converts a file with a word on every line into a json object
 def file_to_json(word_list):
-    scrambles = {}
+    data_out = []
 
     for i in range(len(word_list)):
         word = word_list[i].rstrip()
 
-        start_index = 0
-        end_index = len(word)
-        letters = list(word)
-        print('------' + word + '-------')
+        print('------{0}-------'.format( word ))
         perms = permutations(word)
 
         for perm in perms:
-            perm = list_to_string(perm)
-            if perm in scrambles:
-                scrambles[perm].append(word)
-            else:
-                scrambles[perm] = [word]
-    return scrambles
+            entry = {}
+            entry['permutation'] = list_to_string(perm)
+            entry['word'] = word
+
+            data_out.append( entry )
+            scrambles_db.update_one(entry, { '$set' : entry }, upsert=True)
+
+    #scrambles_db.update_many(data_out, upsert=True)
+
+    time.sleep(20)
+
+    return data_out
+
+def get_database(connection_string:str, db_name:str ):
+    client = MongoClient(connection_string)
+
+    return client[db_name]
 
 
 
@@ -161,24 +169,37 @@ def file_to_json(word_list):
 TEST = 0
 PRODUCTION = 1
 FILES = ['words_short', 'words']
+DBs = ['unscramble-test', 'unscramble']
+
+enviro = TEST
 
 # ===============
 # Commands
 # ===============
 
+# init mongodb
+mongodb_connection = "mongodb://localhost:27017"
+
+scrambles_mongo = get_database(mongodb_connection, DBs[enviro])
+scrambles_db = scrambles_mongo['scrambles']
+
+print('------------ MongoDB Connection Made {0}'.format(scrambles_db))
+
+#time.sleep(10000)
 
 # split files
-file_name = FILES[PRODUCTION]
+file_name = FILES[enviro]
 src_file_path = '{0}/../data/'.format(sys.path[0])
 target_file_path = '{0}/../data/{1}'.format(sys.path[0], file_name)
 file_extension = 'csv'
 split_max = 3
 max_word_length = 6
 process_count = 2
-process_index = 1
+process_index = 0
 
 
 #split_file(file_name, src_file_path, target_file_path, file_extension, split_max, max_word_length)
+
 try:
     process_count = int(sys.argv[1])
     process_index = int(sys.argv[2])
@@ -203,4 +224,4 @@ src_file.close()
 files_to_json(directory_name, data, target_file_path, process_index, process_count )
 
 print('------- nothing else ------')
-time.sleep(1000000)
+#time.sleep(1000000)
